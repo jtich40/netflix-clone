743b1ee38306f7f792eca2c264b3540d042d99b6
Jared Tichacek <jared.tichacek@gmail.com>
2023-08-16 14:15:01 -0500

    setup server and mongodb connection

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+}
+);
