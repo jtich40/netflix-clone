6c1bf2b46a0e7d5778fb9a89cca9b79b18841681
Jared Tichacek <jared.tichacek@gmail.com>
2023-08-22 12:36:50 -0500

    added admin panel, configured new user data for graph, updated sidebar with movies header and icon

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,8 +20,8 @@ router.get("/", verify, async (req, res) => {
     const query = req.query.new
     if (req.user.isAdmin) {
         try {
-            // sort by id in descending order and limit to 10
-            const users = query ? await User.find().sort({ _id: -1 }).limit(10) : await User.find()
+            // sort by id in descending order and limit to 5
+            const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find()
             res.status(200).json(users)
         } catch (err) {
             res.status(500).json(err)
